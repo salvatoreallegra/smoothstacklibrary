5c573ade638971082de2b278805554eead600ef6
Salvatore Allegra <salallegra@gmail.com>
2020-05-20 18:50:51 -0700

    Finish initial top level menu logic

--- library.py
+++ library.py
@@ -1,21 +1,34 @@
-print("Hello library")
+
+MAIN_LIBRARIAN_CHOICE = 1
+MAIN_ADMINISTRATOR_CHOICE = 2
+MAIN_BORROWER_CHOICE = 3
+QUIT_CHOICE = 4
 
 
-def displayMainMenu():
+def main():
+    choice = 0
+    while choice != QUIT_CHOICE:
+        display_main_menu()
+        choice = int(input('Enter your Selection: \n'))
+        if choice == MAIN_LIBRARIAN_CHOICE:
+            print("Librarian Choice\n")
+        elif choice == MAIN_ADMINISTRATOR_CHOICE:
+            print("Main Admin Choice\n")
+        elif choice == MAIN_BORROWER_CHOICE:
+            print("Main Borrower Choice\n")
+        elif choice == QUIT_CHOICE:
+            print('Exiting the program. . .\n')
+        else:
+            print('Error: invalid selection...\n')
+
+
+def display_main_menu():
     print("---Welcome to the Smooth Stack Library Management System---")
     print("Which Category of User are You?")
     print("1) Librarian?")
     print("2) Administrator?")
     print("3) Borrower?")
-    menuSelection = input("Input 1, 2 or 3 ")
-    return menuSelection
+    print("4) Quit")
 
 
-def libraryScreen1():
-    print("\n Library Screen 1 \n")
-
-
-menuSelection = int(displayMainMenu())
-
-if menuSelection == 1:
-    libraryScreen1()
+main()
