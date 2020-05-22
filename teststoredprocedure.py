a433ad64bf4c6b009d73153d12814da5f3f88600
Salvatore Allegra <salallegra@gmail.com>
2020-05-22 15:26:52 -0700

    Fix stored procedure, it's now working

--- teststoredprocedure.py
+++ teststoredprocedure.py
@@ -8,12 +8,17 @@ try:
     cnx = conn.getConnection()
     myCursor = cnx.cursor()
 
-    args = [1, 'Shady Hollow', 'WunderBar']
+    args = ['1', 'Shady Hollow', 'WunderBar', ]
     myCursor.callproc(
         'PROC_UPDATE_LIBRARY_BRANCH', args)
 
     # for result in myCursor.stored_results():
     #     print(result.fetchall())
+
+    if cnx.is_connected():
+        print('still connected')
+
+    cnx.commit()
 
 
 except:
